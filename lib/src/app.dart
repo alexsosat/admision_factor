a6f9c68987045acb53c58170f5972cd65d1fe59b
Alex <1080810@alumno.um.edu.mx>
2021-09-27 11:52:49 -0500

    change app language default

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale('en', ''),
+            Locale('es', ''),
           ],
 
           // Use AppLocalizations to configure the correct application title
